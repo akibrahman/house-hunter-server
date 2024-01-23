d1b82a787ee950dcb24e975aa40af1659bf1ddf7
akibrahman <akibrahman5200@gmail.com>
2024-01-23 20:00:14 +0600

    Server side completed for authentication

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -7,7 +7,7 @@ const IndexController = async (req, res) => {
   res.send("House Hunter Server is Running");
 };
 
-//! Addin new User To DB
+//! Adding new User To DB
 const AddUserController = async (req, res) => {
   const user = await req.body;
   const existingUser = await UserModel.find({ email: user.email });
@@ -26,7 +26,7 @@ const AddUserController = async (req, res) => {
       { email: user.email },
       process.env.ACCESS_TOKEN_SECRET,
       {
-        expiresIn: "5h",
+        expiresIn: "1h",
       }
     );
     const registeredUser = await UserModel.create({
@@ -39,8 +39,77 @@ const AddUserController = async (req, res) => {
         secure: true,
         sameSite: "none",
       })
-      .send({ msg: "User Registered & Token created", user: registeredUser });
+      .send({
+        msg: "User Registered & Token created",
+        user: registeredUser,
+        success: true,
+      });
   });
 };
 
-export { AddUserController, IndexController };
+//! Find User from DB
+const FindUserController = async (req, res) => {
+  const token = req?.cookies?.HouseHunterToken;
+  if (!token) {
+    return res.send({ msg: "No Token", status: false });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
+    if (err) {
+      return res.send({ msg: "Expired or wrong token", status: false });
+    }
+    console.log(decoded);
+    const user = await UserModel.find({ email: decoded.email });
+    res.send({ user: user[0], status: true });
+  });
+};
+
+//! Login User from web
+const LogInUserController = async (req, res) => {
+  const user = await req.body;
+  const data = await UserModel.find({ email: user.email });
+  if (data.length == 0) {
+    res.send({ msg: "Email is incorrect", status: false });
+    return;
+  }
+  bcrypt.compare(user.password, data[0].password, (err, result) => {
+    if (err) {
+      res.send({ msg: "Something went wrong", status: false, err });
+    } else if (result) {
+      const token = jwt.sign(
+        { email: data[0].email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res
+        .cookie("HouseHunterToken", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ msg: "Password is correct", status: true, user: data[0] });
+    } else {
+      res.send({ msg: "Password is incorrect", status: false });
+    }
+  });
+};
+
+//! Logout User from web
+const LogOutUserController = async (req, res) => {
+  res
+    .clearCookie("HouseHunterToken", {
+      maxAge: 0,
+      secure: true,
+      sameSite: "none",
+    })
+    .send({ success: true });
+};
+
+export {
+  AddUserController,
+  FindUserController,
+  IndexController,
+  LogInUserController,
+  LogOutUserController,
+};
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { AddUserController } from "../Controllers/Controllers.js";
+import {
+  AddUserController,
+  FindUserController,
+  LogInUserController,
+  LogOutUserController,
+} from "../Controllers/Controllers.js";
 
 export const UserRoute = express.Router();
 
 //! For Adding User
 UserRoute.post("/add", AddUserController);
+UserRoute.get("/find", FindUserController);
+UserRoute.get("/logout", LogOutUserController);
+UserRoute.post("/login", LogInUserController);
